be10fc17e880804139df286e8f45af2eadc36ece
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2023-02-23 23:41:07 +0100

    comment

--- script/planetInfo.js
+++ script/planetInfo.js
@@ -1,12 +1,13 @@
+// -------------- IF THERE IS TIME -------------------
+// * Add searched images from unsplash as img gallery for each planet
+
 planet = JSON.parse(localStorage.getItem("planets"))
 planet = planet[0]
-console.log(planet)
 
 document.getElementById("title").innerText = planet.name
 document.getElementById("latin").innerText = planet.latinName
 document.getElementById("description").innerText = planet.desc
-document.getElementById("size").innerHTML = planet.circumference
-+ " km"
+document.getElementById("size").innerHTML = planet.circumference + " km"
 document.getElementById("days").innerHTML = planet.orbitalPeriod + " dygn"
 document.getElementById("axel").innerHTML = planet.rotation + " dygn"
 document.getElementById("day_temp").innerHTML = planet.temp.day + " C"
